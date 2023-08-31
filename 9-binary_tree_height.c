ae4c8168ff24dc78a08c7abac34f5d438b731755
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-31 18:31:37 +0300

    fix betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,12 +7,12 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    static size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-    if (!tree || ((!tree->left) && (!tree->right)))
-        return (0);
+	if (!tree || ((!tree->left) && (!tree->right)))
+		return (0);
 
-    left = binary_tree_height(tree->left) + 1;
-    right = binary_tree_height(tree->right) + 1;
-    return ((left >= right) ? left : right);
+	left = binary_tree_height(tree->left) + 1;
+	right = binary_tree_height(tree->right) + 1;
+	return ((left >= right) ? left : right);
 }
