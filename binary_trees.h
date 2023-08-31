3d6a6423551a2f2fe60842ef8758d1686521010d
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-31 22:08:05 +0300

    upload

--- binary_trees.h
+++ binary_trees.h
@@ -56,5 +56,5 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 
-int binary_height(const binary_tree_t *tree);
+
 #endif
