d7168f2ebb62a587cfd640f0e47df7ed5450a50d
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-30 19:01:19 -0700

    test insert at left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,22 +1,25 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
+/**
+ * @parent: a pointer to the node to insert the left-child in
+ */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	bst_t *newNode = NULL;
-	old_left_child = NULL;
+	binary_tree_t *newNode = NULL;
 
 	newNode = binary_tree_node(parent, value); 
 
 	if (!newNode || !parent)
 		return (NULL);
 
-	if (parent && !parent->left)
+	if (!parent->left)
 		parent->left = newNode;
-	else if (parent && parent->left)
+	else if (parent->left)
 	{
-		old_left_child = parent->left;
-		newNode->left = parent->left;
+
+		newNode->left = parent->left;	
 		parent->left = newNode;
 	}
 	
