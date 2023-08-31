4feb86000ff5e38d22bfb943a9a2273982df4ab2
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-31 18:24:51 +0300

    upload

--- elwazeer/9-binary_tree_height.c
+++ elwazeer/9-binary_tree_height.c
@@ -1,4 +1,4 @@
-#include "binary_trees"
+#include "binary_trees.h"
 
 /**
  * binary_tree_height - measures the height of a binary tree
@@ -7,8 +7,12 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (!tree)
+    static size_t left = 0, right = 0;
+
+    if (!tree || ((!tree->left) && (!tree->right)))
         return (0);
 
-    
+    left = binary_tree_height(tree->left) + 1;
+    right = binary_tree_height(tree->right) + 1;
+    return ((left >= right) ? left : right);
 }
