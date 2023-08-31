22e159427ac112800e258d1603c49dec4f40fdc2
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-31 16:21:29 +0300

    adding if parent is null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,6 +12,9 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (!parent)
+		return (NULL);
+
 	if (parent->left == NULL)
 	{
 		parent->left = binary_tree_node(parent, value);
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,6 +13,9 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	if (!parent)
+		return (NULL);
+
 	if (parent->right == NULL)
 	{
 		parent->right = binary_tree_node(parent, value);
