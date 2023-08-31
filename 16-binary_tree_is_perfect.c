750685650ab37b8cce42627b0f1628e67a861a49
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-31 21:54:58 +0300

    adding hanika

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,23 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the height.
+ * Return: If tree is NULL, your function must return 0
+*/
+static int binary_height(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (!tree || ((!tree->left) && (!tree->right)))
+		return (0);
+
+	left = binary_height(tree->left);
+	right = binary_height(tree->right);
+	return ((left >= right) ? left + 1 : right + 1);
+}
+
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
@@ -15,8 +33,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if ((!tree->left) && (!tree->right))
 		return (1);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	left = binary_height(tree->left);
+	right = binary_height(tree->right);
 
 	if (left == right)
 		return (binary_tree_is_perfect(tree->left) &
