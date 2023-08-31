d16c22a578483f490765073ba3d9cd3e697bbc64
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-31 18:55:10 +0300

    alg calc size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the size.
+ * Return: If tree is NULL, your function must return 0
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+	return (left + right + 1);
+}
