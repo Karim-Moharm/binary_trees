d0bedc3e61f8a83140f34ae331704613936eaa59
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-31 12:15:01 -0700

    test perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,21 @@
 #include "binary_trees.h"
 
+/**
+ * binary_height - measures the height of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the height.
+ * Return: If tree is NULL, your function must return 0
+*/
+int binary_height(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (!tree || ((!tree->left) && (!tree->right)))
+		return (0);
+
+	left = binary_height(tree->left);
+	right = binary_height(tree->right);
+	return ((left >= right) ? left + 1 : right + 1);
+}
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -8,7 +24,7 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	// int left = 0, right = 0;
+	int left = 0, right = 0;
 
 	if (!tree)
 		return (0);
@@ -16,28 +32,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if ((!tree->left) && (!tree->right))
 		return (1);
 
-	// left = binary_height(tree->left);
-	// right = binary_height(tree->right);
+	left = binary_height(tree->left);
+	right = binary_height(tree->right);
 
-	// if (left == right)
-	// 	return (binary_tree_is_perfect(tree->left) &
-	// 			binary_tree_is_perfect(tree->right));
+	if (left == right)
+		return (binary_tree_is_perfect(tree->left) &
+				binary_tree_is_perfect(tree->right));
 
-	
-	// if (tree->left && tree->right)
-	// {
-	// 	left = binary_tree_is_perfect(tree->left);
-	// 	right = binary_tree_is_perfect(tree->right);
-
-	// 	if (left == right)
-	// 		return (binary_tree_is_perfect(tree->left) &
-	// 			binary_tree_is_perfect(tree->right));
-	// 	else
-	// 		return (0);
-
-	// }
-		// return (binary_tree_is_perfect(tree->left) &
-		// 		binary_tree_is_perfect(tree->right));
 	return (0);
-
 }
