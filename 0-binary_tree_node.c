4dbab0698d529d6be013585a5ca358de2c15f403
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2023-08-31 15:43:35 +0300

    test create node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,6 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     if (node == NULL)
         return (NULL);
 
+    node->parent = NULL;
     node->left = NULL;
     node->right = NULL;
 
