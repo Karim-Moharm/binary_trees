5b7ca8f4c88819a6a50363cbba39b94489271650
Karim-Moharm <karimmoharm222@gmail.com>
2023-08-31 08:08:58 -0700

    test height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,11 +5,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t lh = 0;
 	size_t rh = 0;
 
-	if (!tree)
+	if (!tree || ((!tree->left) && (!tree->right))) 
 		return (0);
 
 	lh = binary_tree_height(tree->left);
 	rh = binary_tree_height(tree->right);
 
-	return lh > rh ? lh + 1 : rh + 1;
+/*	return lh > rh ? lh + 1 : rh + 1;
+*/
+	if (lh > rh)
+		return lh + 1;
+	else
+		return rh + 1;
 }
